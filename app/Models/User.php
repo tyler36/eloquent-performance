5847cec76886149b5da19054f119c00fe7acf9c3
tyler36 <7234392+tyler36@users.noreply.github.com>
2022-09-26 11:35:26 +0900

    derived table

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,11 +55,29 @@ class User extends Authenticatable
             ->filter()
             ->each(function ($term) use ($query) {
                 $term = $term . '%';
-                $query->where(function ($query) use ($term) {
-                    $query
-                        ->where('first_name', 'like', $term)
-                        ->orWhere('last_name', 'like', $term)
-                        ->orWhereIn('company_id', Company::query()->where('name', 'like', $term)->pluck('id'));
+                // 6 queries, 6.69ms, 5.58ms,
+                // $query->where(function ($query) use ($term) {
+                //     $query
+                //         ->where('first_name', 'like', $term)
+                //         ->orWhere('last_name', 'like', $term)
+                //         ->orWhereIn('company_id', Company::query()->where('name', 'like', $term)->pluck('id'));
+                // });
+                // 3 queries, 18.3ms
+                $query->whereIn('id', function ($query) use ($term) {
+                    $query->select('id')
+                    ->from(function ($query) use ($term) {
+                        $query->select('id')
+                            ->from('users')
+                            ->where('first_name', 'like', $term)
+                            ->orWhere('last_name', 'like', $term)
+                            ->union(
+                                $query->newQuery()
+                                ->select('users.id')
+                                ->from('users')
+                                ->join('companies', 'companies.id', '=', 'users.company_id')
+                                ->where('companies.name', 'like', $term)
+                            );
+                    }, 'matches');
                 });
             });
     }
